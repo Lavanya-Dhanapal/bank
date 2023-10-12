2c7de9c1409b33a99522f90a80a57f8dab3518bd
Lavanya-Dhanapal <144147134+Lavanya-Dhanapal@users.noreply.github.com>
2023-10-12 23:08:43 +0530

    Update BST_Node.cpp

--- DSAproject/BST_Node.cpp
+++ DSAproject/BST_Node.cpp
@@ -1,20 +1,12 @@
 
 # include "BST_Node.h"
 
-BST_Node:: BST_Node()
+BST_Node:: BST_Node() : name(""), address(""), account_number(0), password(0), balance(0)
 {
-	left = nullptr;
-	right = nullptr;
-	name = "";
-    adress = "";
-    account_number = 0;
-    password = 0;
-    balance = 0;
 }
 BST_Node:: BST_Node(string name, string adress, int accountno, int password, int balance)
 {
-	left = nullptr;
-	right = nullptr;
+	
 	this->name = name;
 	this->account_number = accountno;
 	this->adress = adress;
