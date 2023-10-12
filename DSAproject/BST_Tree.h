d0d0c98979bdbed15dc75bda46a14f523abfefde
Lavanya-Dhanapal <144147134+Lavanya-Dhanapal@users.noreply.github.com>
2023-10-12 22:16:17 +0530

    Update BST_Tree.h

--- DSAproject/BST_Tree.h
+++ DSAproject/BST_Tree.h
@@ -1,25 +1,30 @@
 #pragma once
-# include "BST_Node.h"
-# include "Hashtable.h"
-# include <stdio.h>
+#include "BST_Node.h"
+#include "Hashtable.h"
+#include <vector>
+#include <string>
+
 class BST_Tree
 {
-	vector <int> v;
-	
+private:
+    BST_Node *Root;
+    std::vector<int> v;
+
 public:
-	BST_Tree();
-	Hashtable h;
-	BST_Node *Root;
-	void add_Account(string, string, int, int, int);
-	BST_Node* BST_Tree::delete_Account(BST_Node *, int);
-	void withdraw(int,int);
-	void deposit(int,int);
-	void editaccount_byAdmin();
-	void transfer(int,int,int);
-	void transaction_history();
-	void findMax(BST_Node*);
-	void load_Server();
-	void update_server(BST_Node *);
-	BST_Node* search(BST_Node*,int);
-	void printoinfo(BST_Node*);
+    BST_Tree();
+    void add_Account(std::string, std::string, int, int, int);
+    BST_Node *delete_Account(BST_Node *, int);
+    void withdraw(int, int);
+    void deposit(int, int);
+    void editaccount_byAdmin();
+    void transfer(int, int, int);
+    void transaction_history();
+    void findMax(BST_Node *);
+    void load_Server();
+    void update_server(BST_Node *);
+    BST_Node *search(BST_Node *, int);
+    void printoinfo(BST_Node *);
+
+private:
+    Hashtable h;
 };
